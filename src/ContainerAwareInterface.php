cae9a8505d00256dc69142fcdf04b7e3baf41de8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-06-17 13:49:44 +0200

    Update ContainerAwareInterface.php

--- src/ContainerAwareInterface.php
+++ src/ContainerAwareInterface.php
@@ -1,10 +1,23 @@
 <?php
-
 namespace Bermuda;
 
 use Psr\Container\ContainerInterface;
 
+/**
+ * Interface for objects that can receive a dependency injection container.
+ *
+ * This interface is intended for classes that need to gain access to a
+ * dependency injection container after their creation. Usually used in
+ * Service Locator pattern or for container injection into services that
+ * are not created through the container.
+ */
 interface ContainerAwareInterface
 {
-    public function setContainer(ContainerInterface $container): void ;
+    /**
+     * Sets the dependency injection container for the object.
+     *
+     * @param ContainerInterface $container The dependency injection container
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container): void;
 }
